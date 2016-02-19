5e765104cb769fa7bdb285fbeeeca55ef758ef52
James McGregor <jay911144@gmail.com>
2016-02-19 08:25:11 -0600

    Initial Commit

--- download.php
+++ download.php
@@ -19,7 +19,7 @@
 
   URL: <?php echo $_POST["url"]; ?>
   <?php
-  $location = "C:/wamp/www/files/Anime/"
+  $location = "C:/wamp/www/files/Anime/";
   $out = str_ireplace("https://kissanime.com/anime/", "", $_POST["url"]);
   $out = str_ireplace("https://kissanime.to/anime/", "", $_POST["url"]);
   echo "<br>";
